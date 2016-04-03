f1277089f2d26e44a255898ee054c003230664bc
Edward <angertrail@hotmail.com>
2016-04-03 16:58:37 +0800

    Design improvement

--- createItem.php
+++ createItem.php
@@ -33,48 +33,54 @@
 			</form>
 				';*/
 		echo '<h2>Create A New Item</h2>
-			<form id="createForm" method="post" role="form" style"display: none;">
-			<div class="form-group">
-				<input type="text" name="itemName" id="itemName" class="form-control" placeholder="Item Name" value="">
-			</div>
-			<div class="form-group row">
-			<div class="col-sm-4">
-				<label>Item Type:</label>
-				<select name="itemType" required>
-					<option value="appliances">Appliance</option>
-					<option value="tools">Tool</option>
-					<option value="furniture">Furniture</option>
-					<option value="books">Books</option>
-					<option value="others">Others</option>
-				</select>
-			</div>
-			<div class="col-sm-4">
-				<label> Need to Pay: </label>
-				<select name="fee" required>
-					<option value=1>Yes</option>
-					<option value=0>No</option>
-				</select>
-			</div>
-			</div>
-			<div class="form-group">
-				<p>Description</p>
-				<textarea rows="4" cols="50" name="itemDesc" id="itemDesc" form="createForm" placeholder="No more than 100 characters"></textarea>
-			</div>
-			<div class="form-group">
-				<label>Pick-Up Point</label>
-				<input type="text" name="pickUp" id="pickUp" class="form-control" placeholder="Pick-Up Point" value="">
-			</div>
-			<div class="form-group">
-				<label>Return Location</label>
-				<input type="text" name="retL" id="retL" class="form-control" placeholder="Return Point" value="">
-			</div>
-			<div class="form-group">
-				<input type="file" name="imagefile" accept="image/*" id="imagefile">
-			</div>
-			<div class="form-group">
-				<input type="submit" name="itemSubmit" id="itemSubmit" class="form-control btn btn-success" value="Submit">
-			</div>
-			</form>';
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<form id="createForm" method="post" role="form" style"display: none;">
+							<div class="form-group">
+								<input type="text" name="itemName" id="itemName" class="form-control" placeholder="Item Name" value="">
+							</div>
+							<div class="form-group row">
+							<div class="col-sm-4">
+								<label>Item Type:</label>
+								<select name="itemType" required>
+									<option value="appliances">Appliance</option>
+									<option value="tools">Tool</option>
+									<option value="furniture">Furniture</option>
+									<option value="books">Books</option>
+									<option value="others">Others</option>
+								</select>
+							</div>
+							<div class="col-sm-4">
+								<label> Need to Pay: </label>
+								<select name="fee" required>
+									<option value=1>Yes</option>
+									<option value=0>No</option>
+								</select>
+							</div>
+							</div>
+							<div class="form-group">
+								<p>Description</p>
+								<textarea rows="4" cols="50" name="itemDesc" id="itemDesc" form="createForm" placeholder="No more than 100 characters"></textarea>
+							</div>
+							<div class="form-group">
+								<label>Pick-Up Point</label>
+								<input type="text" name="pickUp" id="pickUp" class="form-control" placeholder="Pick-Up Point" value="">
+							</div>
+							<div class="form-group">
+								<label>Return Location</label>
+								<input type="text" name="retL" id="retL" class="form-control" placeholder="Return Point" value="">
+							</div>
+							<div class="form-group">
+								<input type="file" name="imagefile" accept="image/*" id="imagefile">
+							</div>
+							<div class="form-group">
+								<input type="submit" name="itemSubmit" id="itemSubmit" class="form-control btn btn-success" value="Submit">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>';
 			
 		if (isset($_POST['itemSubmit'])) {
 			$itemName = $_POST['itemName'];
@@ -98,8 +104,6 @@
 		$query = "INSERT INTO item VALUES ('$email','$itemType', '$itemID', '$feeFlag', '$itemName', '$pickUp', '$retL', now(), '$itemDesc', '1')";
 
 		$result = pg_query($dbconn, $query);
-		
-		var_dump($result);
 	?>
 	<div class="text-left">
 		<a href="retrieveInfo.php" class="btn btn-success" role="button">Back to Main Page</a>
