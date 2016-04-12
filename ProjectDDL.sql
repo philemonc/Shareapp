89f041cd5088943a0c616b511a3797fa5c786fb3
philemonc <chanphilemon@gmail.com>
2016-04-12 08:57:40 +0800

    finishing touches

---
+++ ProjectDDL.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE member (
+name VARCHAR (64) NOT NULL,
+email VARCHAR(256) PRIMARY KEY,
+password VARCHAR(16) NOT NULL,
+address VARCHAR (256) NOT NULL,
+contactNumber INT NOT NULL,
+adminFlag BIT NOT NULL
+);
+
+CREATE TABLE item (
+email VARCHAR(64) REFERENCES member(email) ON DELETE CASCADE,
+type CHAR(10) CHECK(type='tools' OR type='appliances' OR type='furnitures' OR type='books'),
+itemID INT PRIMARY KEY,
+feeFlag BIT NOT NULL,
+itemName VARCHAR(256) NOT NULL,
+pickupLocation VARCHAR(256) NOT NULL,
+returnLocation VARCHAR(256) NOT NULL,
+availableDate DATE NOT NULL,
+description VARCHAR(256),
+availabilityFlag BIT NOT NULL
+);
+
+CREATE TABLE bidding (
+name VARCHAR(64),
+email VARCHAR(256) REFERENCES member(email) ON DELETE CASCADE,
+feeAmount MONEY NOT NULL,
+itemID INT REFERENCES item(itemID) ON DELETE CASCADE,
+itemName VARCHAR(256),
+PRIMARY KEY (email, itemID),
+dateTime TIMESTAMP,
+successbid BIT NOT NULL,
+pendingstatus BIT NOT NULL
+);
+
+CREATE TABLE loan (
+borrower VARCHAR(256) REFERENCES member(email) ON DELETE CASCADE,
+lender VARCHAR(256) REFERENCES member(email) ON DELETE CASCADE,
+itemID INT REFERENCES item(itemID) ON DELETE CASCADE,
+borrowDate DATE,
+returnDate DATE,
+CHECK(returnDate>=borrowDate),
+PRIMARY KEY (borrower, lender, itemID)
+);
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ CS2102 Database Systems Project Group 9
 	for loan or they can scroll through the available stuffs so that they can borrow it. As soon 
 	as the bids for an item is collected, the best bid is determined by the owner of the item. 
 	Aside from users, administrators of this web application can create, modify and remove items.
+	
+	The application is built on Bitnami WAPP (Windows, Apache, PostgreSQL, PHP) Stack which can 
+	be downloaded from https://bitnami.com/stack/wapp/installer. Once installed, load the 
+	SQL Data Definition Langauge in ProjectDDL.sql into the PostgreSQL database and run the appplication.
